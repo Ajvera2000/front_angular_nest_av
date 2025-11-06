16aa28ec21f77a0dce203f5ef52f0ccf5937291b
Angelo Vera <ajb.vera@yavirac.edu.ec>
2025-11-06 18:39:09 -0500

    componentes

--- src/app/admin/admin-routing-module.ts
+++ src/app/admin/admin-routing-module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Perfil } from './components/perfil/perfil';
+import { Categoria } from './inventario/components/categoria/categoria';
+import { Layout } from './layout/layout';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: 'perfil',
+        component: Perfil
+      },
+      {
+        path: 'categoria',
+        component: Categoria
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin-module').then(m => m.AdminModule)
+  },
+  {
+    path: '',
+    redirectTo: 'admin',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'admin'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
