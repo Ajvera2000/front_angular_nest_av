ce49f0b99e44ec5ac03f9e2898444113d045178c
MigueSaint <amg.sosa@yavirac.edu.ec>
2025-11-07 07:29:18 -0500

    clase 14_d_3

--- src/app/auth/components/login/login.ts
+++ src/app/auth/components/login/login.ts
@@ -2,31 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router'; // ✅ Router de Angular
 
 @Component({
   selector: 'app-login',
-  standalone: true, // necesario porque estás usando imports
+  standalone: true,
   templateUrl: './login.html',
-  styleUrls: ['./login.scss'], // plural correcto
-  imports: [JsonPipe] // habilita el pipe "json" en el template
+  styleUrls: ['./login.scss'],
+  imports: [JsonPipe]
 })
 export class Login {
-  private http = inject(HttpClient);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private http = inject(HttpClient); // ✅ Inyectamos HttpClient
 
   loginForm = new FormGroup({
-    email: new FormControl("ajb.vera@yavirac.edu.ec", [Validators.email, Validators.required]),
-    password: new FormControl("123456", Validators.required)
+    email: new FormControl('ajb.vera@yavirac.edu.ec', [Validators.email, Validators.required]),
+    password: new FormControl('123456', Validators.required)
   });
 
   funIngresar() {
-    this.http.get("https://jsonplaceholder.typicode.com/todos").subscribe(
+    this.http.get('https://jsonplaceholder.typicode.com/todos').subscribe(
       (res) => {
         console.log(res);
+        this.router.navigate(['/admin']); // Navega a /admin
       },
       (error) => {
         console.log(error);
       }
     );
-    // alert("Ingresando...");
   }
 }
