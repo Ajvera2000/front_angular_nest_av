da617d88dea7387418e3cb54e224ba162201b8be
Angelo Vera <ajb.vera@yavirac.edu.ec>
2025-11-06 19:20:26 -0500

    diapositiva 20

--- src/app/auth/components/login/login.ts
+++ src/app/auth/components/login/login.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss',
 })
 export class Login {
+  
+  loginForm=new FormGroup({
+    email:new FormControl('ajb.vera@yavirac.edu.ec'),//valores por defecto
+    password:new FormControl('123456'),//valores por defecto
+})
 
+funIngresar(){
+  alert("Ingresando...")
+}
 }
